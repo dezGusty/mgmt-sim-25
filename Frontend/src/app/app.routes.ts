570c5f5f11e2206f6ec75f08964e125e0e115a31
NastaseTeodor <nastase.teo34@gmail.com>
2025-07-18 11:37:42 +0300

    [update]resolved some errors on the forgot password page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AdminMainPage } from './components/admin/main-page/main-page';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 import { User } from './components/user/user';
+import { ForgotPassword } from './components/forgot-password/forgot-password';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'admin', component: AdminMainPage },
-  { path: 'login', component: Login },
-  { path: 'user', component: User },
-  { path: '**', redirectTo: '/' }
+    { path: '', component: Home },
+    { path: 'admin', component: AdminMainPage },
+    { path: 'login', component: Login },
+    { path: 'user', component: User },
+    { path: 'forgot-password', component: ForgotPassword },
+    { path: '**', redirectTo: '/' }
 ];
--- Frontend/src/app/components/login/login.ts
+++ Frontend/src/app/components/login/login.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-login',
+  imports: [RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   goBack() {
     this.router.navigate(['/']);
+  }
+
+  goToForgotPassword() {
+    this.router.navigate(['/forgot-password']);
   }
 }
