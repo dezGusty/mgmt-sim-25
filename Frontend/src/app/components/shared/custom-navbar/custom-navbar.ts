30f99432f560a88314481b0f2a2990b3c8b87a7b
DenisTatu <tatu_denis@yahoo.com>
2025-08-19 14:37:30 +0300

    [UPDATE] - Added the user's name in the header next to the logout button + stylization

--- Frontend/src/app/components/shared/custom-navbar/custom-navbar.ts
+++ Frontend/src/app/components/shared/custom-navbar/custom-navbar.ts
@@ -12,14 +12,49 @@ export class CustomNavbar implements OnInit {
   showAddRequestForm = false;
   userRoles: string[] = [];
   showHomeButton = true;
+  userEmail: string = '';
 
-  constructor(private router: Router, private authService: Auth) {}
+  constructor(private router: Router, private authService: Auth) { }
 
   ngOnInit(): void {
-    this.loadUserRoles();
+    this.loadUserData();
   }
 
-  async loadUserRoles(): Promise<void> {
+  getPrimaryRole(): string {
+    if (this.userRoles.includes('Admin')) return 'Admin';
+    if (this.userRoles.includes('HR')) return 'HR';
+    if (this.userRoles.includes('Manager')) return 'Manager';
+    if (this.userRoles.includes('Employee')) return 'Employee';
+    return this.userRoles[0] || 'User';
+  }
+
+  getRoleColor(): string {
+    const role = this.getPrimaryRole();
+    switch (role) {
+      case 'Admin':
+        return 'bg-blue-600 text-white';
+      case 'HR':
+        return 'bg-purple-600 text-white ';
+      case 'Manager':
+        return 'bg-yellow-300 text-white';
+      case 'Employee':
+        return 'bg-green-600 text-white';
+      default:
+        return 'bg-gray-600 text-white';
+    }
+  }
+
+  getRoleTextColor(): string {
+    const role = this.getPrimaryRole();
+    switch (role) {
+      case 'Manager':
+        return 'text-gray-900';
+      default:
+        return 'text-white';
+    }
+  }
+
+  async loadUserData(): Promise<void> {
     try {
       const response = await fetch('https://localhost:7275/api/Auth/me', {
         credentials: 'include',
@@ -27,14 +62,18 @@ export class CustomNavbar implements OnInit {
 
       if (response.ok) {
         const data = await response.json();
-        if (data && data.roles) {
-          this.userRoles = data.roles;
-          this.showHomeButton = this.userRoles.length > 1;
+        if (data) {
+          this.userEmail = data.email || '';
+          if (data.roles) {
+            this.userRoles = data.roles;
+            this.showHomeButton = this.userRoles.length > 1;
+          }
         }
       }
     } catch (error) {
-      console.error('Error loading user roles:', error);
+      console.error('Error loading user data:', error);
       this.showHomeButton = false;
+      this.userEmail = '';
     }
   }
 
@@ -54,7 +93,7 @@ export class CustomNavbar implements OnInit {
     });
   }
 
-  goHome(){
+  goHome() {
     this.router.navigate(['/role-selector']);
   }
 }
